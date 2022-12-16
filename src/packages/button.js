253fe22772738c42d7297a26ac84e3559bb63e9d
PAVRamana <ramana.ponnaganti@gmail.com>
2022-12-16 15:07:14 +0800

    Update button.js

--- src/packages/button.js
+++ src/packages/button.js
@@ -1,13 +1,27 @@
 import AddIcon from "@mui/icons-material/Add";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import CallMergeIcon from "@mui/icons-material/CallMerge";
+import CallSplitIcon from "@mui/icons-material/CallSplit";
 
-function ButtonCmp({ onClickAdd, onClickUp, onClickDown }) {
+function ButtonCmp({
+  onClickAdd,
+  onClickUp,
+  onClickDown,
+  onCellUpClick,
+  onCellDownClick,
+}) {
   return (
-    <div style={{ display: "flex", marginBottom: "10px" }}>
-      <AddIcon onClick={onClickAdd} />
+    <div style={{ display: "flex", marginBottom: "12px" }}>
+      <AddIcon
+        onClick={() => {
+          onClickAdd();
+        }}
+      />
       <ArrowUpwardIcon onClick={onClickUp} />
       <ArrowDownwardIcon onClick={onClickDown} />
+      <CallMergeIcon onClick={onCellUpClick} />
+      <CallSplitIcon onClick={onCellDownClick} />
     </div>
   );
 }
