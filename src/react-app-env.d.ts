bc12b0c22a1d55bbc9cd7188faca3b888559efe2
Ramana <ramana.ponnaganti@gmail.com>
2024-06-19 22:09:56 +0800

    change

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,56 @@
-/// <reference types="react-scripts" />
+{
+  "workbench.colorTheme": "Default Dark+",
+  "[javascript]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "[json]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "editor.formatOnSave": true,
+  "[typescript]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    "editor.tabSize": 2
+  },
+  "[typescriptreact]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    "editor.tabSize": 2
+  },
+  "[scss]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "[html]": {
+    "editor.defaultFormatter": "vscode.html-language-features"
+  },
+  "workbench.startupEditor": "none",
+  "diffEditor.ignoreTrimWhitespace": false,
+  "[css]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "workbench.editorAssociations": {
+    "*.jar": "default",
+    "*.zip": "default"
+  },
+  "timeline.excludeSources": [],
+  "redhat.telemetry.enabled": true,
+  "[xml]": {
+    "editor.defaultFormatter": "fabianlauer.vs-code-xml-format"
+  },
+  "[vue]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "[javascriptreact]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode"
+  },
+  "eslint.validate": [
+    "javascript",
+    "svelte",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact"
+  ],
+  "eslint.debug": true,
+  "eslint.nodeEnv": "",
+  "prettier.singleQuote": true,
+  "prettier.jsxSingleQuote": true,
+  "eslint.nodePath": ""
+}
